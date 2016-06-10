b6361a951eb280cc10a7a5eba0174f45454cc49f
Alê Haak <alehaak@gmail.com>
2016-06-09 19:37:45 -0700

    renaming variables

--- Getting and Cleaning Data/Week 3/Quiz week 3.R
+++ Getting and Cleaning Data/Week 3/Quiz week 3.R
@@ -10,22 +10,22 @@
     install.packages("jpeg")
     library("jpeg")
     fileurl2 = 'https://d396qusza40orc.cloudfront.net/getdata%2Fjeff.jpg'
-    dst2 = './data/q2.jpg'
-    download.file(fileurl2, dst2, mode = 'wb')
-    data2 = readJPEG(dst2, native = TRUE)
+    fileName2 = './data/q2.jpg'
+    download.file(fileurl2, fileName2, mode = 'wb')
+    data2 = readJPEG(fileName2, native = TRUE)
     quantile(data2, probs = c(0.3, 0.8))
 
 #Question3
     install.packages("data.table")
     library("data.table")
     fileurl3a = 'https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv'
-    dst3a = './data/q3a.csv'
+    fileName3 = './data/q3a.csv'
     fileurl3b = 'https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv'
-    dst3b = './data/q3b.csv'
-    download.file(fileurl3a, dst3a, mode="wb")
-    download.file(fileurl3b, dst3b, mode="wb")
-    gdp = fread(dst3a, skip=4, nrows = 190, select = c(1, 2, 4, 5), col.names=c("CountryCode", "Rank", "Economy", "Total"))
-    edu = fread(dst3b)
+    fileName3b = './data/q3b.csv'
+    download.file(fileurl3a, fileName3, mode="wb")
+    download.file(fileurl3b, fileName3b, mode="wb")
+    gdp = fread(fileName3, skip=4, nrows = 190, select = c(1, 2, 4, 5), col.names=c("CountryCode", "Rank", "Economy", "Total"))
+    edu = fread(fileName3b)
     merge = merge(gdp, edu, by = 'CountryCode')
     nrow(merge)
     arrange(merge, desc(Rank))[13, Economy]
