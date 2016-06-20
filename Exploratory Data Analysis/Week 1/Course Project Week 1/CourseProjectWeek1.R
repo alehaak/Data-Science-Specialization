2ca2ea4caed522c8134df83e2758992545d62a30
Alê Haak <alehaak@gmail.com>
2016-06-19 17:16:36 -0700

    Exploratory Data Analysis Week 1 with Quiz and Course Project

---
+++ Exploratory Data Analysis/Week 1/Course Project Week 1/CourseProjectWeek1.R
@@ -0,0 +1,50 @@
+#Exploratory Data Analysis Course Project 1 
+#
+#Exctracting and subsetting dataset
+    dataFile <- "./data/hpc.txt"
+    data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+    dataSubset <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+    
+#Setting variables
+    globalActivePower <- as.numeric(dataSubset$Global_active_power)
+    globalReactivePower <- as.numeric(dataSubset$Global_reactive_power)
+    voltage <- as.numeric(dataSubset$Voltage)
+    datetime <- strptime(paste(dataSubset$Date, dataSubset$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+    subMetering1 <- as.numeric(dataSubset$Sub_metering_1)
+    subMetering2 <- as.numeric(dataSubset$Sub_metering_2)
+    subMetering3 <- as.numeric(dataSubset$Sub_metering_3)
+    
+#Plot 1
+    png("./data/plot1.png", width=1024, height=768)
+    hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+    dev.off()
+
+#Plot 2
+    png("./data/plot2.png", width=1024, height=768)
+    plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+    dev.off()
+    
+#Plot 3
+    png("./data/plot3.png", width=1024, height=768)
+    plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
+    lines(datetime, subMetering2, type="l", col="red")
+    lines(datetime, subMetering3, type="l", col="blue")
+    legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+    dev.off()
+    
+#Plot 4
+    png("./data/plot4.png", width=1024, height=768)
+    par(mfrow = c(2, 2)) 
+    #Top Left
+        plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power", cex=0.2)
+    #Top Right
+        plot(datetime, voltage, type="l", xlab="datetime", ylab="Voltage")
+    #Bottom Left
+        plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
+        lines(datetime, subMetering2, type="l", col="red")
+        lines(datetime, subMetering3, type="l", col="blue")
+        legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="o")
+    #Bottom Right
+        plot(datetime, globalReactivePower, type="l", xlab="datetime", ylab="Global_reactive_power")
+    
+    dev.off()
---
+++ Exploratory Data Analysis/Week 1/W1Q.R
@@ -0,0 +1,34 @@
+#100% Exploratory Data Analysis Week 1 Quiz 
+    #1. Which of the following is a principle of analytic graphics?
+        #Show causality, mechanism, explanation
+        #Integrate multiple modes of evidence
+
+    #2. What is the role of exploratory graphs in data analysis?
+        #They are typically made very quickly.
+
+    #3. Which of the following is true about the base plotting system?
+        #Plots are created and annotated with separate functions
+
+    #4. Which of the following is an example of a valid graphics device in R?
+        #The computer screen
+        #A PDF file
+
+    #5. Which of the following is an example of a vector graphics device in R?
+        #SVG
+
+    #6. Bitmapped file formats can be most useful for
+        #Scatterplots with many many points
+
+    #7. Which of the following functions is typically used to add elements to a plot in the base graphics system?
+        #"text()" or "points()"
+
+    #8a. Which function opens the screen graphics device for the Mac?
+        #quartz()
+    #8b.Which function opens the screen graphics device on Windows?
+        #windows()
+
+    #9. What does the 'pch' option to par() control?
+        #the plotting symbol/character in the base graphics system
+
+    #10. If I want to save a plot to a PDF file, which of the following is a correct way of doing that?
+        #Construct the plot on the screen device and then copy it to a PDF file with dev.copy2pdf()
